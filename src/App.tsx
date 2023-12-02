1ba39a0d0dca2a9774d5336fb0260cdb6b74202b
Juboraj Islam Mamun <juborajvai22@gmail.com>
2023-12-02 19:16:23 +0600

    complete login fun

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import Login from "./components/auth/Login";
+import Reg from "./components/auth/Reg";
 import Home from "./components/home/Home";
 import MainLayout from "./layout/MainLayout";
 import { Route, Routes } from "react-router-dom";
@@ -7,9 +9,9 @@ function App() {
     <MainLayout>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Reg />} />
-        <Route path="/user/:id" element={<UserDetails />} /> */}
+        {/* <Route path="/user/:id" element={<UserDetails />} /> */}
         {/* <Route
           path="/my-team"
           element={
